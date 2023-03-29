f8b1d26083123c2cf22b5713c5fc597051d76b5f
AFord99 <129235742+AFord99@users.noreply.github.com>
2023-03-29 18:08:02 -0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Triangle.h"
+using namespace std;
+
+int main() {
+   Triangle triangle1;
+   Triangle triangle2;
+   double base1;
+   double base2;
+   double height1;
+   double height2;
+
+   cin >> base1;
+   cin >> height1;
+   triangle1.SetBase(base1);
+   triangle1.SetHeight(height1);
+      
+
+   cin >> base2;
+   cin >> height2;
+   triangle2.SetBase(base2);
+   triangle2.SetHeight(height2);
+       
+   cout << "Triangle with smaller area:" << endl;
+   
+   if (triangle1.GetArea() < triangle2.GetArea())
+   {
+      triangle1.PrintInfo();
+   }
+   else
+   {
+      triangle2.PrintInfo();
+   }
+   
+   return 0;
+}
